a38c1f1683187976ed3af28179ccb872c13e2e54
Vybekid <felobitto@gmail.com>
2025-07-16 12:41:52 +0300

    done well

--- myfile.py
+++ myfile.py
@@ -1,5 +1,5 @@
-import turtle
-import time
+import turtle 
+import time 
 
 screen = turtle.Screen()
 screen.bgcolor("black")
@@ -7,22 +7,24 @@ screen.title("Michael Jackson")
 
 butterfly_frames = ["A.gif", "B.gif", "C.gif", "D.gif"]
 
-
-for frame in butterfly_frames:
+for frame in butterfly_frames: 
     screen.addshape(frame)
-
 
 butterfly = turtle.Turtle()
 butterfly.penup()
 butterfly.speed("fastest")
-butterfly.goto(-screen.window_width() // 2, 0) 
+butterfly.goto(-screen.window_width() // 2, 0)
 
-frame_index = 0
+frame_index = 0 
 while butterfly.xcor() < screen.window_width() // 2: 
-    butterfly.shape(butterfly_frames[frame_index])
+    butterfly.shape(butterfly_frames[frame_index]) 
     frame_index = (frame_index + 1) % len(butterfly_frames)
     butterfly.forward(15)
     screen.update()
     time.sleep(0.25)
 
 turtle.done()
+
+
+
+
